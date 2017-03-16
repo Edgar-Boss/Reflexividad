b70079aa84a8f659732c23da9c85d61bc4686d7d
Edgar-Boss <edgardiazortiz@gmail.com>
2017-03-16 02:25:16 -0600

    16/03/2017
    Dado un conjunto y su relacion, verifica que la relacion sea reflexiva

---
+++ Reflexividad/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.TextBox();
+            this.A2 = new System.Windows.Forms.TextBox();
+            this.A3 = new System.Windows.Forms.TextBox();
+            this.A4 = new System.Windows.Forms.TextBox();
+            this.R10 = new System.Windows.Forms.TextBox();
+            this.R11 = new System.Windows.Forms.TextBox();
+            this.R20 = new System.Windows.Forms.TextBox();
+            this.R21 = new System.Windows.Forms.TextBox();
+            this.R30 = new System.Windows.Forms.TextBox();
+            this.R31 = new System.Windows.Forms.TextBox();
+            this.R40 = new System.Windows.Forms.TextBox();
+            this.R41 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Resultado = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(35, 19);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(23, 20);
+            this.A1.TabIndex = 0;
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(64, 19);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(23, 20);
+            this.A2.TabIndex = 1;
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(93, 19);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(23, 20);
+            this.A3.TabIndex = 2;
+            // 
+            // A4
+            // 
+            this.A4.Location = new System.Drawing.Point(122, 19);
+            this.A4.Name = "A4";
+            this.A4.Size = new System.Drawing.Size(23, 20);
+            this.A4.TabIndex = 3;
+            // 
+            // R10
+            // 
+            this.R10.Location = new System.Drawing.Point(61, 22);
+            this.R10.Name = "R10";
+            this.R10.Size = new System.Drawing.Size(23, 20);
+            this.R10.TabIndex = 5;
+            // 
+            // R11
+            // 
+            this.R11.Location = new System.Drawing.Point(90, 22);
+            this.R11.Name = "R11";
+            this.R11.Size = new System.Drawing.Size(23, 20);
+            this.R11.TabIndex = 6;
+            // 
+            // R20
+            // 
+            this.R20.Location = new System.Drawing.Point(148, 22);
+            this.R20.Name = "R20";
+            this.R20.Size = new System.Drawing.Size(23, 20);
+            this.R20.TabIndex = 7;
+            // 
+            // R21
+            // 
+            this.R21.Location = new System.Drawing.Point(177, 22);
+            this.R21.Name = "R21";
+            this.R21.Size = new System.Drawing.Size(23, 20);
+            this.R21.TabIndex = 8;
+            // 
+            // R30
+            // 
+            this.R30.Location = new System.Drawing.Point(238, 22);
+            this.R30.Name = "R30";
+            this.R30.Size = new System.Drawing.Size(23, 20);
+            this.R30.TabIndex = 9;
+            // 
+            // R31
+            // 
+            this.R31.Location = new System.Drawing.Point(267, 22);
+            this.R31.Name = "R31";
+            this.R31.Size = new System.Drawing.Size(23, 20);
+            this.R31.TabIndex = 10;
+            // 
+            // R40
+            // 
+            this.R40.Location = new System.Drawing.Point(326, 22);
+            this.R40.Name = "R40";
+            this.R40.Size = new System.Drawing.Size(23, 20);
+            this.R40.TabIndex = 11;
+            // 
+            // R41
+            // 
+            this.R41.Location = new System.Drawing.Point(355, 22);
+            this.R41.Name = "R41";
+            this.R41.Size = new System.Drawing.Size(23, 20);
+            this.R41.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(396, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 32);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Resultado
+            // 
+            this.Resultado.Location = new System.Drawing.Point(25, 152);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(100, 20);
+            this.Resultado.TabIndex = 14;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.A4);
+            this.groupBox1.Controls.Add(this.A3);
+            this.groupBox1.Controls.Add(this.A2);
+            this.groupBox1.Controls.Add(this.A1);
+            this.groupBox1.Location = new System.Drawing.Point(9, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(182, 52);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Conjunto";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.R21);
+            this.groupBox2.Controls.Add(this.R20);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.R11);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.R10);
+            this.groupBox2.Controls.Add(this.R30);
+            this.groupBox2.Controls.Add(this.R31);
+            this.groupBox2.Controls.Add(this.R40);
+            this.groupBox2.Controls.Add(this.R41);
+            this.groupBox2.Location = new System.Drawing.Point(12, 65);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(410, 73);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Relación";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "A={";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "}";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "R={";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "(";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(132, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "(";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(222, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "(";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(312, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "(";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(116, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(10, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = ")";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(381, 25);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(10, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = ")";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(397, 25);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(11, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "}";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(296, 24);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(10, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = ")";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(206, 24);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(10, 13);
+            this.label12.TabIndex = 25;
+            this.label12.Text = ")";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 261);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox A1;
+        private System.Windows.Forms.TextBox A2;
+        private System.Windows.Forms.TextBox A3;
+        private System.Windows.Forms.TextBox A4;
+        private System.Windows.Forms.TextBox R10;
+        private System.Windows.Forms.TextBox R11;
+        private System.Windows.Forms.TextBox R20;
+        private System.Windows.Forms.TextBox R21;
+        private System.Windows.Forms.TextBox R30;
+        private System.Windows.Forms.TextBox R31;
+        private System.Windows.Forms.TextBox R40;
+        private System.Windows.Forms.TextBox R41;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Resultado;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Reflexividad/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            int[] A=new int[4];
+            int[,] R = new int[4, 2];
+
+            A[0] = int.Parse(A1.Text);
+            A[1] = int.Parse(A2.Text);
+            A[2] = int.Parse(A3.Text);
+            A[3] = int.Parse(A4.Text);
+
+            R[0, 0] = int.Parse(R10.Text);
+            R[0, 1] = int.Parse(R11.Text);
+            R[1, 0] = int.Parse(R20.Text);
+            R[1, 1] = int.Parse(R21.Text);
+            R[2, 0] = int.Parse(R30.Text);
+            R[2, 1] = int.Parse(R31.Text);
+            R[3, 0] = int.Parse(R40.Text);
+            R[3, 1] = int.Parse(R41.Text);
+
+
+            int cont = 0;
+            for (int k = 0; k < 4; k++)
+            {
+
+                for (int j = 0; j < 4; j++)
+                {
+
+                    if (A[k] == R[j, 0] && A[k] == R[j, 1])
+                    {
+                        cont++;
+
+                    }
+
+
+
+                }
+
+            }
+
+            if (cont == 4)
+                Resultado.Text = "Es reflexiva";
+            else
+                Resultado.Text = "No es reflexiva";
+
+
+
+        }
+    }
+}
